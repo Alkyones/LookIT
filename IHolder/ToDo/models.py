5f69acdf61fb0dd09c1cd1fefb05f7ec48b1691b
Alkyones <lifeonshort@gmail.com>
2022-04-14 23:56:35 +0300

    Models have been created

--- IHolder/ToDo/models.py
+++ IHolder/ToDo/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoList(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+class TodoListItem(models.Model):
+    content = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    todo_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'is_completed': self.is_completed
+        }
