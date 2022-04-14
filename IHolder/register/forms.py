3d45d8209c271a8fe3329b959ba1eb40547fec30
Alkyones <lifeonshort@gmail.com>
2022-04-14 23:46:24 +0300

    V1.1 Added register template and related urls with fixed bugs

---
+++ IHolder/register/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class NewRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        help_texts = {
+            'username': None,
+            'email': None,
+            'password1': None,
+            'password2': None,
+        }
---
+++ IHolder/register/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .forms import NewRegisterForm
+from django.contrib import messages
+
+
+
+
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = NewRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            messages.error(request, 'Form is not valid')
+    else:
+        form = NewRegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def profile(request):
+    return render(request, 'registration/profile.html')
