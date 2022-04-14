5cd4b65f01211921edfc2ee63207828db6baa0c5
Alkyones <lifeonshort@gmail.com>
2022-04-15 00:05:33 +0300

    forms have been created using models.py

---
+++ IHolder/ToDo/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import TodoList, TodoListItem
+
+
+
+class TodoListForm(forms.ModelForm):
+    class Meta:
+        model = TodoList
+        fields = ['name']
+
+class TodoListItemForm(forms.ModelForm):
+    class Meta:
+        model = TodoListItem
+        fields = ['content', 'completed']
